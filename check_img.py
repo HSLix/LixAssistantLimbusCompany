0432a54ab3b8af37fcb9ffa3c26f11149174e038
陆爻齐-LuYaoQi <l0123i456@163.com>
2025-07-05 00:25:32 +0800

    feat: accelerate skip event

--- check_img.py
+++ check_img.py
@@ -6,13 +6,14 @@ eye = get_eye()
 
 # while True:
 eye.captureScreenShot()
-c = eye.templateMatch("shop_sell_ego_resource.png", recognize_area=[0, 0, 0, 0], threshold=0, is_show_result=True)
-#c = eye.templateMatch("acquire_ego_new.png", recognize_area=[660, 190, 350, 100], threshold=0, is_show_result=True)
-#c = eye.templateMatch("shop_sell_ego_resource.png", recognize_area=[0, 0, 0, 0], threshold=0, is_show_result=True)
-#c = eye.templateMatch("shop_sell_ego_resource.png", recognize_area=[180, 450, 540, 260], threshold=0, is_show_result=True)
+# c = eye.templateMatch("shop_sell_ego_resource.png", recognize_area=[0, 0, 0, 0], threshold=0, is_show_result=True)
+
 # c = [[100, 110]]
 # c += eye.templateMultiMatch("acquire_ego_gift.png", threshold=0.7, recognize_area=[0, 0, 0, 0], is_show_result=False)
 # c.clear()
+
+c = eye.rgbDetection([1330, 840])
+
 print(c)
 sleep(0.1)
 
